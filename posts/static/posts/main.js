4a1343a3d20bc42adef70550d9c4a924f0e396b1
RobinNor114 <robinwagubi@gmail.com>
2025-04-09 23:40:32 -0400

    Got posts Objects with Ajax and added a spinner

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,1 +1,36 @@
 console.log('hello world')
+
+const helloWorldBox =  document.getElementById('hello-world')
+const postsBox =  document.getElementById('posts-box')
+const spinnerBox =  document.getElementById('spinner-box')
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log('success', response.text)
+        helloWorldBox.textContent = response.text
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
+
+$.ajax({
+    type: 'GET',
+    url: '/data',
+    success: function(response){
+        console.log(response) 
+        const data = response.data;
+        spinnerBox.classList.add('not-visible')
+        console.log(data);
+        data.forEach(el => {
+            postsBox.innerHTML += `
+                ${el.title} - <b>${el.body}</b><br>
+            `;
+        });
+    },
+    error: function(error){
+        console.log(error)
+    }
+})
